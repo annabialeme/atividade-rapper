dca1a43345688b865707e699c263cdc1c5bc1e01
anna leme <162629023+annabialeme@users.noreply.github.com>
2024-10-16 21:46:08 -0300

    feat: rota para deletar um rapper

--- src/routes/rappers.routes.js
+++ src/routes/rappers.routes.js
@@ -97,6 +97,10 @@ const rappers = rappers.find((rappers) => rappers.id == id);
     }); 
   };
 
+// Rota para deletar um rapper
+rappersRoutes.delete("/:id", (req, res) => {
+    const { id } = req.params;
 
+});
   
 export default rappersRoutes;
